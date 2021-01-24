8cb4806c1f6ba7b0f2a9b7f301db85a5fefca13f
Timothy Brathwaite <timothyb0912@gmail.com>
2021-01-24 14:51:37 -0800

    Add short tree on grass

--- src/learn_arcade_work/scratch_work/drawing_samples.py
+++ src/learn_arcade_work/scratch_work/drawing_samples.py
@@ -13,6 +13,15 @@ arcade.start_render()
 
 # Drawing code goes here
 
+# Draw some grass
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+
+# Draw a tree stump on top of the grass
+arcade.draw_lrtb_rectangle_filled(100, 120, 350, 300, arcade.csscolor.SIENNA)
+
+# Draw a top on top of the tree
+arcade.draw_circle_filled(110, 350, 30, arcade.csscolor.GREEN)
+
 # Finish drawing
 arcade.finish_render()
 
