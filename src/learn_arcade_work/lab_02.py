d9f2aa4e002518fe95addb737429ffe85da144c0
Timothy Brathwaite <timothyb0912@gmail.com>
2021-01-31 15:03:48 -0800

    Requirements for Lab 02
      * Configure pyprojroot so I can access
      images in `reports/figures`.
      * Store prototyping plan and image plan.
      * Store 'paper' prototype of the image.

---
+++ src/learn_arcade_work/lab_02.py
@@ -0,0 +1,27 @@
+"""
+This is my drawing for Lab 02.
+
+For this lab, I will focus on the drawing that is most practically useful to me
+at the moment:
+a computational graph of a multinomial, neural network classifier.
+
+This image needs to be complete by February 19th, giving me 3 Sundays
+(counting Valentine's day).
+
+
+What's the minimum-valuable-product?
+  * A neural network with two hidden layers
+  * Final Layer = Y1, Y2, Y3, ...
+  * Orientation is landscape such that covariates are on the left,
+  and computation flows to the right towards the outputs
+
+
+What prototypes are useful and why?
+  * type: paper / powerpoint-pdf
+    rationale: documents the image of what I actually want.
+  * type: arcade, 4-neurons_2-neurons_1-output
+    rationale: |
+    teaches me the basics of how to make the image in arcade,
+    gives me a base to experiment with to design a easy to use code to make the
+    MVP drawing.
+"""
