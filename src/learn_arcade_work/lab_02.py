6c3035ee41abe1d79a6ad04954435cfffe90cc40
Timothy Brathwaite <timothyb0912@gmail.com>
2021-02-07 14:58:05 -0800

    Began drawing afro avatar

--- src/learn_arcade_work/lab_02.py
+++ src/learn_arcade_work/lab_02.py
@@ -1,27 +1,109 @@
 """
 This is my drawing for Lab 02.
 
-For this lab, I will focus on the drawing that is most practically useful to me
-at the moment:
-a computational graph of a multinomial, neural network classifier.
+For this lab, I will focus on a drawing that is practically useful to me
+at the moment: an image of a person.
 
-This image needs to be complete by February 19th, giving me 3 Sundays
-(counting Valentine's day).
-
-
-What's the minimum-valuable-product?
-  * A neural network with two hidden layers
-  * Final Layer = Y1, Y2, Y3, ...
-  * Orientation is landscape such that covariates are on the left,
-  and computation flows to the right towards the outputs
-
-
-What prototypes are useful and why?
-  * type: paper / powerpoint-pdf
-    rationale: documents the image of what I actually want.
-  * type: arcade, 4-neurons_2-neurons_1-output
-    rationale: |
-    teaches me the basics of how to make the image in arcade,
-    gives me a base to experiment with to design a easy to use code to make the
-    MVP drawing.
+A person image is generically useful, and I plan to use it in causal diagrams.
 """
+from typing import Tuple
+
+import arcade
+import attr
+
+
+@attr.s
+class Afro:
+    center_x: int = attr.ib(default=300)
+    center_y: int = attr.ib(default=300)
+    outer_radius: int = attr.ib(default=100)
+    inner_radius: int = attr.ib(default=60)
+    inner_y_offset: int = attr.ib(default=-20)
+    hair_color: Tuple[int] = attr.ib(default=arcade.csscolor.BLACK)
+
+    def _draw_outer_circle(self):
+        arcade.draw_circle_outline(
+            center_x=self.center_x,
+            center_y=self.center_y,
+            radius=self.outer_radius,
+            color=self.hair_color,
+        )
+
+    def _draw_inner_circle(self):
+        arcade.draw_circle_outline(
+            center_x=self.center_x,
+            center_y=self.center_y + self.inner_y_offset,
+            radius=self.inner_radius,
+            color=self.hair_color,
+        )
+
+    def _draw_hair_outer(self):
+        pass
+
+    def _draw_hair_inner(self):
+        pass
+
+    def draw(self):
+        self._draw_outer_circle()
+        self._draw_inner_circle()
+        self._draw_hair_outer()
+        self._draw_hair_inner()
+
+
+@attr.s
+class Face:
+    def draw(self):
+        pass
+
+
+@attr.s
+class Body:
+    def draw(self):
+        pass
+
+
+@attr.s
+class Avatar:
+    afro: Afro = attr.ib()
+    face: Face = attr.ib()
+    body: Body = attr.ib()
+
+    def draw(self):
+        afro.draw()
+        face.draw()
+        body.draw()
+
+
+def make_drawing(
+    avatar: Avatar,
+    window_width: int = 600,
+    window_height: int = 600,
+    background_color: Tuple[int] = arcade.color.EARTH_YELLOW,
+    title: str = "Afro-Avatar",
+) -> None:
+    arcade.open_window(window_width, window_height, title)
+
+    # Set the background color
+    arcade.set_background_color(background_color)
+
+    # Begin drawing
+    arcade.start_render()
+
+    avatar.draw()
+
+    # Finish drawing
+    arcade.finish_render()
+
+    # Run the commands and keep the window open.
+    arcade.run()
+
+    return
+
+
+if __name__ == "__main__":
+
+    afro = Afro()
+    face = Face()
+    body = Body()
+    avatar = Avatar(afro=afro, face=face, body=body)
+    make_drawing(avatar)
